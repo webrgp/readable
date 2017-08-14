b90aff93ee66322e012fcd493ef631a1c755c996
Rodrigo Passos <rodrigopassos@mac.com>
2017-08-14 19:21:14 -0400

    feat: add API helper

---
+++ src/utils/ReadableAPI.js
@@ -0,0 +1,21 @@
+const api = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:5001'
+
+let token = localStorage.token
+
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+export const getCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+
+export const getPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
