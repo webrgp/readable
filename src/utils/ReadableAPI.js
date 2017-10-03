3ca2201593126a4e28eebcb7471ec8b8b33d61fd
Rodrigo Passos <rodrigopassos@mac.com>
2017-10-03 13:38:39 -0400

    fix api

--- src/utils/ReadableAPI.js
+++ src/utils/ReadableAPI.js
@@ -1,6 +1,6 @@
 import Moment from 'moment';
 
-const api = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:5001'
+const api = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:3001'
 
 let token = localStorage.token
 
