8983f4b922fc7a345a0def77bb29ce9bb5a74e19
Rodrigo Passos <rodrigopassos@mac.com>
2017-10-06 11:10:29 -0400

    feat: add logger

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 import App from  './App';
 import reducers from './reducers';
 import './index.css';
@@ -15,7 +16,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, logger))
 );
 
 ReactDOM.render(
