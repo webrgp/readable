3d2f5e095c20031e7898e6bc6246c5600a5dfbb8
Rodrigo Passos <rodrigopassos@mac.com>
2017-10-06 11:36:46 -0400

    chore: remove logger

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger'
 import App from  './App';
 import reducers from './reducers';
 import './index.css';
@@ -16,7 +15,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware(thunk, logger))
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
