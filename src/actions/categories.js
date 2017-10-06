119f6440d29ca979c061ececc172a65206f04861
Rodrigo Passos <rodrigopassos@mac.com>
2017-10-06 11:46:01 -0400

    chore: clean up code

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -1,7 +1,11 @@
 import * as ReadableAPI from '../utils/ReadableAPI';
 
 export const LOAD_CATEGORIES = 'LOAD_CATEGORIES';
-export const SELECT_CATEGORY = 'SELECT_CATEGORY';
+
+export const loadCategories = categories => ({
+  type: LOAD_CATEGORIES,
+  categories
+});
 
 // Fetch categories from API
 export const fetchCategories = () => dispatch => (
@@ -9,15 +13,3 @@ export const fetchCategories = () => dispatch => (
       .fetchCategories()
       .then(categories => dispatch(loadCategories(categories)))
 );
-
-export const loadCategories = categories => ({
-  type: LOAD_CATEGORIES,
-  categories
-});
-
-export const selectCategory = (selectedCategory) => {
-  return {
-    type: SELECT_CATEGORY,
-    selectedCategory
-  }
-}
