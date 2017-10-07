12a8ada0a44ea60401791700eb483063dd1e098c
Rodrigo Passos <rodrigopassos@mac.com>
2017-10-07 16:51:07 -0400

    chore: add delete handle

--- src/components/PostControls/index.js
+++ src/components/PostControls/index.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import VoteControl from '../VoteControl';
 import CommentsCountCountrol from '../CommentsCountCountrol';
+import { faPencilAlt, faTrashAlt } from '@fortawesome/fontawesome-free-solid'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import './PostControls.css';
 
 export default class PostControls extends Component {
+
+  handleDeletePost = () => {
+    console.log('Delete post id: ' + this.props.post.id);
+  }
+
   render () {
 
     const { post } = this.props;
 
     return (
-      <div className="btn-toolbar">
-        <CommentsCountCountrol parentId={post.id} />
-        <VoteControl entry={post} />
+      <div className="PostControls">
+        <div className="btn-toolbar">
+          <CommentsCountCountrol parentId={post.id} />
+          <VoteControl entry={ post } />
+          <div className="PostControls--edit-remove mr-0 ml-auto">
+            <button className="btn btn-light"><FontAwesomeIcon iconDefinition={faPencilAlt} /></button>
+            <button 
+              className="btn btn-light"
+              onClick={ () => { this.handleDeletePost() } }
+            ><FontAwesomeIcon iconDefinition={faTrashAlt} /></button>
+          </div>
+        </div>
       </div>
     );
   }
